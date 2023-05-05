51f4c9b1f83eb2e47b8971a39d0fb703635b2542
Maxim Prestwich <maximprestwich1@gmail.com>
2023-05-05 10:16:19 +0100

    add update user by id route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -13,11 +13,33 @@ router.get("/api/users", async (req, res) => {
   }
 });
 
+router.get("/api/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({ user: user });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 router.post("/api/users", async (req, res) => {
   try {
     const userInformation = req.body.user;
-    const newUser = User.create(userInformation);
+    const newUser = await User.create(userInformation);
     res.status(200).json({ user: newUser });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/api/users/:id", async (req, res) => {
+  try {
+    console.log(req.body);
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    console.log(updatedUser);
+    res.status(200).json({ user: updatedUser });
   } catch (error) {
     res.status(500).json(error);
   }
