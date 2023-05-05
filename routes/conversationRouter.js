65edb1602287ec21c5ba4e8fb5734532283d0d7f
Maxim Prestwich <maximprestwich1@gmail.com>
2023-05-05 13:57:14 +0100

    move conversation router functions to controller file

--- routes/conversationRouter.js
+++ routes/conversationRouter.js
@@ -1,57 +1,25 @@
 import express from "express";
-import mongoose from "mongoose";
 import { Conversation } from "../models/conversation.js";
+import {
+  getConversationsByUserId,
+  createConversation,
+  updateConversationWithMessage,
+  deleteConversation,
+} from "../controllers/conversationsController.js";
 
 const router = express.Router();
 
 // create a conversation between two users
-router.post("/api/conversations", async (req, res) => {
-  try {
-    const conversation = await Conversation.create(req.body.conversation);
-    res.status(200).json({ conversation: conversation });
-  } catch (error) {
-    res.status(200).json(error);
-  }
-});
+router.post("/api/conversations", createConversation);
 
 // get all conversations for a user
 // id is a user id
-router.get("/api/conversations/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-    const conversation = await Conversation.find({
-      $or: [{ participantOne: id }, { participantTwo: id }],
-    });
-    res.status(200).json({ conversation: conversation });
-  } catch (error) {
-    res.status(200).json(error);
-  }
-});
+router.get("/api/conversations/:id", getConversationsByUserId);
 
 // add message to conversation
 // id is the conversation id
-router.put("/api/conversations/:id", async (req, res) => {
-  try {
-    const conversation = await Conversation.findByIdAndUpdate(
-      req.params.id,
-      {
-        $push: { messages: req.body.message },
-      },
-      { new: true }
-    );
-    res.status(200).json({ conversation: conversation });
-  } catch (error) {
-    res.status(200).json(error);
-  }
-});
+router.put("/api/conversations/:id", updateConversationWithMessage);
 
-router.delete("/api/conversations/:id", async (req, res) => {
-  try {
-    const conversation = await Conversation.findByIdAndDelete(req.params.id);
-    res.status(200).json({ conversation: conversation });
-  } catch (error) {
-    res.status(200).json(error);
-  }
-});
+router.delete("/api/conversations/:id", deleteConversation);
 
 export default router;
