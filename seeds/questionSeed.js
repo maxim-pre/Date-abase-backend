cf09490fc22eeb8cb316c127be73dcb65b85880e
Louise Jones <louisejones94@live.co.uk>
2023-05-05 14:23:45 +0100

    Add seed files

---
+++ seeds/questionSeed.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import currentDB from "../config/db.js";
+import { Question } from "../models/question.js";
+
+const db = mongoose.connection;
+mongoose.connect(currentDB);
+
+const questions = [
+    {
+        questionText: "What's the best tech stack?",
+        possibleAnswers: ["MEAN", "MERN", "MEVN", "LAMP", "PERN", "Ruby on Rails-based", "Python-based", "Serverless"],
+        questionLive: true
+    },
+    {
+        questionText: "SQL or NoSQL?",
+        possibleAnswers: ["SQL", "NoSQL", "Don't care"],
+        questionLive: true
+    },
+    {
+        questionText: "When should comments be used?",
+        possibleAnswers: ["As much as possible", "Only to explain ambiguities", "Only when a programmer isn't good enough to explain themselves"],
+        questionLive: true
+    },
+    {
+        questionText: "White space should be...",
+        possibleAnswers: ["Used sparingly", "Used everywhere"],
+        questionLive: true
+    },
+    {
+        questionText: "When you're coding you...",
+        possibleAnswers: ["Shut the door and remove distractions", "Get brainstorming with others"],
+        questionLive: true
+    }
+]
+
