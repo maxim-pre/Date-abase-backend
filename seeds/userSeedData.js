b4240bcc4a23ac1f7471aa39b770acc2a7fcfa6d
Maxim Prestwich <maximprestwich1@gmail.com>
2023-05-05 15:51:53 +0100

    added user seed file

---
+++ seeds/userSeedData.js
@@ -0,0 +1,30 @@
+export const users = [
+  {
+    firstName: "Maxim",
+    lastName: "Prestwich",
+    password: "1234",
+    username: "maxim123",
+    interestedInGender: "F",
+  },
+  {
+    firstName: "Patrick",
+    lastName: "Mallery",
+    password: "1234",
+    username: "Pat123",
+    interestedInGender: "F",
+  },
+  {
+    firstName: "Louise",
+    lastName: "Jones",
+    password: "1234",
+    username: "Louise123",
+    interestedInGender: "M",
+  },
+  {
+    firstName: "Simeon",
+    lastName: "Oye",
+    password: "1234",
+    username: "Simeon123",
+    interestedInGender: "F",
+  },
+];
